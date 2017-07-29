6045a958425fc68bbea8c01f08f0a50f72ca88d6
Manas Karekar <powerslave12r@gmail.com>
2017-07-29 15:18:58 -0400

    - Add a function to trim alpha in textmate tm themes. - Apply that function to textmate profile reads.

--- profiles/tm.py
+++ profiles/tm.py
@@ -4,6 +4,12 @@ from ir import IR
 
 import plistlib
 
+def trim_alpha(color, trim = True):
+	if trim and len(color[1:]) > 6:
+		color = color.strip('#')
+		color = '#' + color[2:]
+	return color
+
 def read(source_file):
 	try:
 		lines = plistlib.readPlist(source_file)
@@ -22,26 +28,26 @@ def read(source_file):
 
 	ir = IR()
 	ir.name					= lines['name']
-	ir.fgcolor				= settings[0]['settings'].get('foreground', default_light)
-	ir.bgcolor				= settings[0]['settings'].get('background', default_light)
+	ir.fgcolor				= trim_alpha(settings[0]['settings'].get('foreground', default_light))
+	ir.bgcolor				= trim_alpha(settings[0]['settings'].get('background', default_light))
 
-	ir.keyword1				= settings[7]['settings'].get('foreground', default_dark)
-	ir.keyword2				= settings[7]['settings'].get('foreground', default_dark)
-	ir.keyword3				= settings[7]['settings'].get('foreground', default_dark)
-	ir.keyword4				= settings[7]['settings'].get('foreground', default_dark)
+	ir.keyword1				= trim_alpha(settings[7]['settings'].get('foreground', default_dark))
+	ir.keyword2				= trim_alpha(settings[7]['settings'].get('foreground', default_dark))
+	ir.keyword3				= trim_alpha(settings[7]['settings'].get('foreground', default_dark))
+	ir.keyword4				= trim_alpha(settings[7]['settings'].get('foreground', default_dark))
 
-	ir.comment1				= settings[1]['settings'].get('foreground', default_dark)
-	ir.digit				= settings[3]['settings'].get('foreground', default_dark)
-	ir.operator				= settings[3]['settings'].get('foreground', default_dark) #TODO: get the right one
-	ir.function				= settings[12]['settings'].get('foreground', default_dark)
+	ir.comment1				= trim_alpha(settings[1]['settings'].get('foreground', default_dark))
+	ir.digit				= trim_alpha(settings[3]['settings'].get('foreground', default_dark))
+	ir.operator				= trim_alpha(settings[3]['settings'].get('foreground', default_dark)) #TODO: get the right one
+	ir.function				= trim_alpha(settings[12]['settings'].get('foreground', default_dark))
 
-	ir.literal1				= settings[2]['settings'].get('foreground', default_light)
-	ir.literal2				= settings[2]['settings'].get('foreground', default_light)
-	ir.literal3				= settings[2]['settings'].get('foreground', default_light)
-	ir.caretColor			= settings[0]['settings'].get('caret', default_light)
-	ir.selectionColor		= settings[0]['settings'].get('selection', default_light)
-	ir.eolMarkerColor		= settings[0]['settings'].get('foreground', default_light)
-	ir.lineHighlightColor 	= settings[0]['settings'].get('lineHighlight', default_light)
+	ir.literal1				= trim_alpha(settings[2]['settings'].get('foreground', default_light))
+	ir.literal2				= trim_alpha(settings[2]['settings'].get('foreground', default_light))
+	ir.literal3				= trim_alpha(settings[2]['settings'].get('foreground', default_light))
+	ir.caretColor			= trim_alpha(settings[0]['settings'].get('caret', default_light))
+	ir.selectionColor		= trim_alpha(settings[0]['settings'].get('selection', default_light))
+	ir.eolMarkerColor		= trim_alpha(settings[0]['settings'].get('foreground', default_light))
+	ir.lineHighlightColor 	= trim_alpha(settings[0]['settings'].get('lineHighlight', default_light))
 
 	return ir
 
